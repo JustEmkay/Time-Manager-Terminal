c4bf4317ac15cde1a0e66f58bf3a5eef0873cd09
JustEmkay <just.mk.things@gmail.com>
2024-09-14 19:36:23 +0530

    added changes in journal

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 import curses,json,pytz
 from curses import wrapper
+from curses.textpad import Textbox,rectangle
 from datetime import datetime , time as t
 import time
 
@@ -13,8 +14,8 @@ def journal_data() -> dict:
     temp : dict = {
                 'productivity' : 0,
                 'mood' : 0,
-                'agenda_not_done' : [],
-                'agenda_done' : [],
+                'agenda_not_done' : ['Go for walk'],
+                'agenda_done' : ['do home work'],
                 'thankful' : ['im still alive', 'no need to use rope'],
                 'lessons' : "",
                 'sucks' : 'sleep early/ wake up at 5am',
@@ -59,7 +60,9 @@ y : int = 5
 
 def menu(stdscr,options,ms_index,x,y,RED_WHITE) -> None:
 
-    stdscr.clear()
+    # stdscr.clear()
+    
+    stdscr.addstr(x-2,y,'Raspi-Terminal-app',curses.COLOR_GREEN)    
     for idx,items in enumerate(options):
         if idx == ms_index:
             stdscr.addstr(idx+x,y,items,RED_WHITE)    
@@ -94,10 +97,10 @@ def journal_menu(stdscr,j_index,RED_WHITE)->None:
     for idx,item in enumerate(journal_options): # idx-> index| item->{}
         for ix in item: # ix-> key of item dict
             if idx == j_index:
-                stdscr.addstr(counter,y,ix+" << EDIT",RED_WHITE) # display heading
+                stdscr.addstr(counter,y,ix+" << EDIT",RED_WHITE | curses.A_STANDOUT) # display heading
                 counter+=1 
             else:
-                stdscr.addstr(counter,y,ix,curses.A_BOLD)
+                stdscr.addstr(counter,y,ix,curses.A_BOLD | curses.A_UNDERLINE)
                 counter+=1
                 
             if idx == 0:
@@ -124,12 +127,43 @@ def journal_menu(stdscr,j_index,RED_WHITE)->None:
                         counter+=1
                         
             else:
-                stdscr.addstr(counter,y,item[ix])
+                if item[ix] == "":
+                    stdscr.addstr(counter,y,'Null')
+                else:
+                    stdscr.addstr(counter,y,item[ix])
+                counter+=1
+                
         
             counter+=1                       
                                    
     # stdscr.refresh()
+
+def journal_edit(stdscr,edit_opt) -> None:
+    stdscr.clear()
+    if edit_opt == 'edit_agenda':
+        # stdscr.addstr(x+1,y,"Edit: Today's Agends") #6 5 
+        # stdscr.addstr(x+2,y,"_____________________________________________") #7 5
         
+        # win = curses.newwin(1,45,9,5)
+        # box = Textbox(win)
+        # rectangle(stdscr,x,y-1,15,50)
+        
+        # stdscr.refresh()
+        # box.edit()
+        # text = box.gather()
+        # stdscr.addstr(17,5,text)
+        
+        # stdscr.getch()
+        pass
+        
+    elif edit_opt == 'edit_thank':
+        pass
+    elif edit_opt == 'edit_lessons':
+        pass
+    elif edit_opt == 'edit_sucks':
+        pass
+    stdscr.refresh()
+            
 def journal(stdscr,RED_WHITE) -> None:
     
     j_index : int = -1
@@ -139,46 +173,56 @@ def journal(stdscr,RED_WHITE) -> None:
         stdscr.addstr(x+1,y,"__________________________________________")
         
         journal_menu(stdscr,j_index,RED_WHITE)
+        
+        stdscr.addstr(x+18,y,"Press Up/Down to select")
+        stdscr.addstr(x+19,y,"Press Enter to Edit")
         stdscr.addstr(x+20,y,"Press q to exit journal")
         stdscr.addstr(x+21,y,"__________________________________________")
-        # stdscr.addstr(x+6,y,str(j_index))
         
         key = stdscr.getkey()
-        # stdscr.addstr(x+15,y,str(key))
-                    
+          
         if key == 'KEY_UP' and j_index > 0:
             j_index -= 1
         elif key == 'KEY_DOWN' and j_index < len(journal_options)-1:
             j_index += 1
         elif key == 'q':
             return 0
+        elif key == 'KEY_ENTER' or key == '\n' or key == '\r':
+            if j_index == 0:
+                journal_edit(stdscr,'edit_agenda')
+            elif j_index == 1:
+                journal_edit(stdscr,'edit_thank')
+            elif j_index == 2:
+                journal_edit(stdscr,'edit_lessons')
+            elif j_index == 3:
+                journal_edit(stdscr,'edit_sucks')
+                    
+        
 
         stdscr.clear()
     stdscr.refresh()
 
 
-
 def main(stdscr):
 
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     RED_WHITE = curses.color_pair(1)
-    GREEN_WHITE = curses.color_pair(2)
+    GREEN_BLACK = curses.color_pair(2)
     
     menu_selector_index : int = 0 
     # x , y = 5,5
     
     while 1:
+        stdscr.clear()
         menu(stdscr,menu_items,menu_selector_index,x,y,RED_WHITE)
         stdscr.addstr(x+6,y,str(menu_selector_index))
         stdscr.addstr(x+6,y,f'Selected : {menu_items[menu_selector_index]}')
 
         key = stdscr.getkey()
-        # stdscr.clear()
         stdscr.addstr(x+6,y,str(key))
         stdscr.refresh()
-        # stdscr.getch()
-                   
+
         if key == 'KEY_UP' and menu_selector_index > 0:
             menu_selector_index -= 1
         elif key == 'KEY_DOWN' and menu_selector_index < len(menu_items)-1:
@@ -192,11 +236,7 @@ def main(stdscr):
             elif menu_items[menu_selector_index] == 'Exit':
                 return 0
                 
-    # for i in range(1,10):
-    #     stdscr.clear()
-    #     stdscr.addstr(f'Saving data :  {i}% completed')
-    #     time.sleep(1)    
-    #     stdscr.refresh()
+
 
 
 wrapper(main)
